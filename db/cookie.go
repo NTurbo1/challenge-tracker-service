66f5dd7a608ba4d714c92a7211ab2def2b2eac02
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2025-10-07 00:04:36 +0500

    Implement creating/initializing CookieRepository: implement cookies csv file parsers.

--- db/cookie.go
+++ db/cookie.go
@@ -2,8 +2,11 @@ package db
 
 import (
 	"os"
+	"strings"
 	"net/http"
 	"fmt"
+	"bufio"
+	"strconv"
 	"github.com/nturbo1/challenge-tracker-service/util"
 )
 
@@ -12,7 +15,8 @@ type CookieRepo struct {
 	Cookies []*http.Cookie
 }
 
-var cookiesCSVHeader = "Name, Value, MaxAge, Secure, HttpOnly, Domain, SameSite"
+const cookiesCSVHeader = "Name, Value, MaxAge, Secure, HttpOnly, Domain"
+const numCookiesCSVCols = 6
 
 func CreateCookieRepo(cookiesFilePath string) (*CookieRepo, error) {
 	fileExists := util.FileExists(cookiesFilePath)
@@ -43,7 +47,84 @@ func CreateCookieRepo(cookiesFilePath string) (*CookieRepo, error) {
 }
 
 func parseCookiesCSVFile(file *os.File) ([]*http.Cookie, error) {
-	return nil, fmt.Errorf("FixMe: Implement parseCookiesCSVFile!")
+	if file == nil {
+		return nil, fmt.Errorf("Can't parse nil file.")
+	}
+
+	cookies := []*http.Cookie{}
+	lineCount := 0
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineCount++
+
+		if lineCount == 1 {
+			if strings.Compare(cookiesCSVHeader, line) != 0 {
+				return nil, fmt.Errorf(
+					"Invalid cookies csv header: %s. Expected to be: %s", line, cookiesCSVHeader,
+				)
+			}
+			fmt.Printf("Line %d (Header): %s\n", lineCount, line)
+			continue
+		}
+
+		fmt.Printf("Line %d: %s\n", lineCount, line)
+		cookiePtr, err := parseCookieCSVLine(line)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("Parsed to: ", *cookiePtr)
+		cookies = append(cookies, cookiePtr)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return cookies, nil
+}
+
+func parseCookieCSVLine(csvLine string) (*http.Cookie, error) {
+	if len(csvLine) == 0 {
+		return nil, fmt.Errorf("Empty line in cookies csv file.")
+	}
+	cols := strings.Split(csvLine, ",")
+	if len(cols) != numCookiesCSVCols {
+		return nil, fmt.Errorf("Invalid row in the cookies csv file: %s", csvLine)
+	}
+
+	maxAge, err := strconv.Atoi(cols[2])
+	if err != nil {
+		fmt.Printf(
+			"Failed to convert cookie maxAge value %s to integer from line %s\n", cols[2], csvLine,
+		)
+		return nil, err
+	}
+	secure, err := strconv.ParseBool(cols[3])
+	if err != nil {
+		fmt.Printf(
+			"Failed to convert cookie secure value %s to boolean from line %s\n", cols[3], csvLine,
+		)
+		return nil, err
+	}
+	httpOnly, err := strconv.ParseBool(cols[4])
+	if err != nil {
+		fmt.Printf(
+			"Failed to convert cookie httpOnly value %s to boolean from line %s\n", cols[4], csvLine,
+		)
+		return nil, err
+	}
+
+	cookiePtr := &http.Cookie{
+		Name: cols[0],
+		Value: cols[1],
+		MaxAge: maxAge,
+		Secure: secure,
+		HttpOnly: httpOnly,
+		Domain: cols[5],
+	}
+
+	return cookiePtr, nil
 }
 
 func findCookie(cookieName string) *http.Cookie {
--- db/persistence.go
+++ db/persistence.go
@@ -6,11 +6,16 @@ var UsersRepository *UserRepo = nil
 var CookieRepository *CookieRepo = nil
 
 const usersCSVFilePath = "db/data/users.csv"
+const cookiesCSVFilePath = "db/data/cookies.csv"
 
 func InitDb() error {
 	var err error
 	UsersRepository, err = CreateUserRepo(usersCSVFilePath)
+	if err != nil {
+		return err
+	}
 
+	CookieRepository, err = CreateCookieRepo(cookiesCSVFilePath)
 	if err != nil {
 		return err
 	}
